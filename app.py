3776783ba93f80c480e7e89411d3dd220180da89
vkedia <vkedia@uwaterloo.ca>
2025-04-10 22:59:50 -0400

    Fix

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Query, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import PlainTextResponse
 from pydantic import BaseModel
+import urllib.parse
 
 app = FastAPI()
 
